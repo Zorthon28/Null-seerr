06a4108659eb6f5c2b0b1af3b4d48c08b5c86dc9
Gustavo Tello <gustavo.tello19@tectijuana.edu.mx>
2026-09-24 10:43:18 -0700

    fix: eliminate background trailer audio conflict when opening fullscreen modal (#22)

--- src/components/MovieDetails/index.tsx
+++ src/components/MovieDetails/index.tsx
@@ -902,6 +902,7 @@ const MovieDetails = ({ movie }: MovieDetailsProps) => {
                 className="z-40 border border-rose-500/40 text-rose-300 hover:bg-rose-600/20"
                 buttonSize="md"
                 onClick={() => {
+                  setIsTrailerEnlarged(false);
                   setModalTrailerKey(trailerVideo?.key);
                   setShowTrailerModal(true);
                 }}
