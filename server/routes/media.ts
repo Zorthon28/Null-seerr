6a8314ce70846f1467a3f0e246320a005f46c247
Zorthon28 <99435781+Zorthon28@users.noreply.github.com>
2026-09-25 09:21:24 -0700

    fix(media): prevent NaN undefined display when bytes is null or zero

--- server/routes/media.ts
+++ server/routes/media.ts
@@ -117,11 +117,11 @@ mediaRoutes.get('/', async (req, res, next) => {
 });
 
 function formatBytes(bytes: number, decimals = 2) {
-  if (bytes === 0) return '0 Bytes';
+  if (!bytes || isNaN(bytes) || bytes <= 0) return '0 Bytes';
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const i = Math.min(Math.floor(Math.log(bytes) / Math.log(k)), sizes.length - 1);
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
